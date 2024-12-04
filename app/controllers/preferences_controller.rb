72b4a2ae8b1f2144c5f2beb4b599940c73f92161
heather stevens <heather.stevens.cm@gmail.com>
2024-12-04 09:40:16 -0700

    kickstart/draft

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,2 +1,14 @@
 class PreferencesController < ApplicationController
+
+  def movies
+    @suggested_movies = fetch_suggested_movies(current_user)
+    render 'pages/preferences_movies'
+  end
+
+  private
+
+  def fetch_suggested_movies(user)
+    # Placeholder to fetch suggested movies
+    Movie.limit(3)
+  end
 end
