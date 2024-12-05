cab9bd88dbc7a3dc897ce59ea33b205ad48f21e1
heather stevens <heather.stevens.cm@gmail.com>
2024-12-05 08:53:42 -0700

    uncommented authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
-  # commented out temporarily
-  def authenticate_user!
-  end
+  before_action :authenticate_user!
 end
