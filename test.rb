f0cf2416fb64cf83ab2f102c4b07b91f8ba1dc4c
David Cazal <da.cazal95@gmail.com>
2024-12-04 20:14:12 -0500

    streamings services seed added

--- test.rb
+++ test.rb
@@ -1,9 +1,24 @@
-require "json"
-require "rest-client"
-
-response = RestClient.get "https://api.watchmode.com/v1/sources/?apiKey=gWw6LOBRd08bUW54mjl0r6j4O5JqW3IzXRAX0jME&types=sub"
-repos = JSON.parse(response)
-
-services = repos.map {|service| service["name"]}.uniq
-
-pp services
+streaming_services_array = [
+  {name: "The Criterion Channel"},
+  {name: "Netflix"},
+  {name: "Hulu"},
+  {name: "Max"},
+  {name: "Prime Video"},
+  {name: "Disney+"},
+  {name: "AppleTV+"},
+  {name: "Paramount+"},
+  {name: "Showtime"},
+  {name: "Crave"},
+  {name: "Peacock Premium"},
+  {name: "Crave Starz"},
+  {name: "STARZ"},
+  {name: "MGM+"},
+  {name: "Now TV"},
+  {name: "BINGE"},
+  {name: "Kanopy"},
+  {name: "Youtube Premium"},
+  {name: "ICI TOU.TV"},
+  {name: "MUBI"}
+]
+sorted_streamings = streaming_services_array.sort_by { |services| services[:name] }
+pp sorted_streamings
