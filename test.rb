6199acd982523c05bb770f7471d7703d86dea118
David Cazal <da.cazal95@gmail.com>
2024-12-05 14:09:20 -0500

    stille processing

--- test.rb
+++ test.rb
@@ -1,23 +1,9 @@
-streaming_services_array = [
-  {name: "The Criterion Channel"},
-  {name: "Netflix"},
-  {name: "Hulu"},
-  {name: "Max"},
-  {name: "Prime Video"},
-  {name: "Disney+"},
-  {name: "AppleTV+"},
-  {name: "Paramount+"},
-  {name: "Showtime"},
-  {name: "Crave"},
-  {name: "Peacock Premium"},
-  {name: "Crave Starz"},
-  {name: "STARZ"},
-  {name: "MGM+"},
-  {name: "Now TV"},
-  {name: "BINGE"},
-  {name: "Kanopy"},
-  {name: "Youtube Premium"},
-  {name: "ICI TOU.TV"},
-  {name: "MUBI"}
-]
-sorted_streamings = streaming_services_array.sort_by { |services| services[:name] }
+require "json"
+require "rest-client"
+
+response = RestClient.get "https://api.watchmode.com/v1/releases/?apiKey=gWw6LOBRd08bUW54mjl0r6j4O5JqW3IzXRAX0jME"
+repos = JSON.parse(response)
+# => repos is an `Array` of `Hashes`.
+
+type = repos["releases"].map { |release| release["type"] }.uniq
+pp type
