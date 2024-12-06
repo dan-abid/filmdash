390e61e6b047ee7a28dd71381054dd941972884d
David Cazal <da.cazal95@gmail.com>
2024-12-06 15:02:31 -0500

    can see 3 titles

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 require "json"
 require "rest-client"
 
-# response = RestClient.get "https://api.watchmode.com/v1/list-titles/?apiKey=gWw6LOBRd08bUW54mjl0r6j4O5JqW3IzXRAX0jME&types=movie&source_ids=203&region=CA&genres=1&release_date_start=20240101&critic_score_high=90&limit=3"
+# response = RestClient.get "https://api.watchmode.com/v1/list-titles/?apiKey="
 # selection = JSON.parse(response)
 
 # pp selection
@@ -15,7 +15,7 @@ require "rest-client"
   # "total_results"=>26}
 
 
-# response = RestClient.get "https://api.watchmode.com/v1/releases/?apiKey=gWw6LOBRd08bUW54mjl0r6j4O5JqW3IzXRAX0jME"
+# response = RestClient.get "https://api.watchmode.com/v1/releases/?apiKey="
 # repos = JSON.parse(response)
 
 # streaming = repos["releases"].map { |stream| "{ source_id: #{stream["source_id"]}, name: #{stream["source_name"]} }" }.uniq
@@ -26,7 +26,7 @@ require "rest-client"
 
 # list title:
 
-# response = RestClient.get "https://api.watchmode.com/v1/title/tt17526714/sources/?apiKey=gWw6LOBRd08bUW54mjl0r6j4O5JqW3IzXRAX0jME&regions=CA"
+# response = RestClient.get "https://api.watchmode.com/v1/title/tt17526714/sources/?apiKey="
 # repos = JSON.parse(response)
 
 # pp repos
