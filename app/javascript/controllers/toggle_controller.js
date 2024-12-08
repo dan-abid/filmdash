8bb29a211a4310fc875803396839c29023b68f69
David Cazal <da.cazal95@gmail.com>
2024-12-08 14:01:59 -0500

    still processing

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,13 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["togglableElement"]
+  static targets = ["movedElement", "appearElement", "divElement"]
 
   connect() {
     console.log("HellO!");
   }
 
   fire () {
-    this.togglableElementTarget.classList.toggle("d-none");
+    this.appearElementTarget.classList.remove("d-none");
+    this.movedElementTarget.classList.remove("justify-content-center")
+    this.movedElementTarget.classList.add("justify-content-between")
+    // this.togglableElementTarget.classList.toggle("d-none");
+
   }
 }
