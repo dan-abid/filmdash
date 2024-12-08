830f431ef9f7e0c6a3a013610065b3f44cee24f4
David Cazal <da.cazal95@gmail.com>
2024-12-08 17:07:09 -0500

    can click on a movie and the infos appear (no click outside)

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,17 +2,45 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["movedElement", "appearElement", "divElement"]
+  static targets = ["appear1Element",
+                    "play1Element",
+                    "appear2Element",
+                    "play2Element",
+                    "appear3Element",
+                    "play3Element",
+                  ]
 
   connect() {
     console.log("HellO!");
   }
 
-  fire () {
-    this.appearElementTarget.classList.remove("d-none");
-    this.movedElementTarget.classList.remove("justify-content-center")
-    this.movedElementTarget.classList.add("justify-content-between")
+  button1 (event) {
+    // event.target.classList.add("movie-card-dark")
+    this.appear1ElementTarget.classList.remove("d-none");
+    this.play1ElementTarget.classList.remove("d-none");
+    this.play1ElementTarget.classList.remove("d-none");
+    event.target.classList.remove("justify-content-center");
+    event.target.classList.add("justify-content-between");
     // this.togglableElementTarget.classList.toggle("d-none");
+  }
 
+  button2 (event) {
+    this.appear2ElementTarget.classList.remove("d-none");
+    this.play2ElementTarget.classList.remove("d-none");
+    this.play2ElementTarget.classList.remove("d-none");
+    event.target.classList.remove("justify-content-center");
+    event.target.classList.add("justify-content-between");
+  }
+
+  button3 (event) {
+    this.appear3ElementTarget.classList.remove("d-none");
+    this.play3ElementTarget.classList.remove("d-none");
+    this.play3ElementTarget.classList.remove("d-none");
+    event.target.classList.remove("justify-content-center");
+    event.target.classList.add("justify-content-between");
+  }
+
+  refresh() {
+    window.location.reload();
   }
 }
