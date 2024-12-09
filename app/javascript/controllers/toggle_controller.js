1bf04c13268cfdbf5c122ec0cf9344e379b38c0c
David Cazal <da.cazal95@gmail.com>
2024-12-09 07:34:20 -0500

    can see 3 movie san dthe details.

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -8,36 +8,37 @@ export default class extends Controller {
                     "play2Element",
                     "appear3Element",
                     "play3Element",
+                    "container1Element",
+                    "container2Element",
+                    "container3Element"
                   ]
 
   connect() {
-    console.log("HellO!");
+    console.log("Hello");
+
   }
 
-  button1 (event) {
+  button1 () {
     // event.target.classList.add("movie-card-dark")
     this.appear1ElementTarget.classList.remove("d-none");
     this.play1ElementTarget.classList.remove("d-none");
-    this.play1ElementTarget.classList.remove("d-none");
-    event.target.classList.remove("justify-content-center");
-    event.target.classList.add("justify-content-between");
+    this.container1ElementTarget.classList.remove("justify-content-center");
+    this.container1ElementTarget.classList.add("justify-content-between");
     // this.togglableElementTarget.classList.toggle("d-none");
   }
 
-  button2 (event) {
+  button2 () {
     this.appear2ElementTarget.classList.remove("d-none");
     this.play2ElementTarget.classList.remove("d-none");
-    this.play2ElementTarget.classList.remove("d-none");
-    event.target.classList.remove("justify-content-center");
-    event.target.classList.add("justify-content-between");
+    this.container2ElementTarget.classList.remove("justify-content-center");
+    this.container2ElementTarget.classList.add("justify-content-between");
   }
 
-  button3 (event) {
+  button3 () {
     this.appear3ElementTarget.classList.remove("d-none");
     this.play3ElementTarget.classList.remove("d-none");
-    this.play3ElementTarget.classList.remove("d-none");
-    event.target.classList.remove("justify-content-center");
-    event.target.classList.add("justify-content-between");
+    this.container3ElementTarget.classList.remove("justify-content-center");
+    this.container3Element.classList.add("justify-content-between");
   }
 
   refresh() {
