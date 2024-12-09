a27fdf1adc992902e99b5eda77a0cdb5fd127537
danial abid <dan.abid91@gmail.com>
2024-12-09 17:46:46 -0500

    update

--- app/models/movie_watchlist.rb
+++ app/models/movie_watchlist.rb
@@ -6,16 +6,4 @@ validates :director, presence: true
 validates :year, presence: true
 validates :rating, presence: true
 validates :streaming_link, presence: true
-
-def self.search(search)
-  if search
-    movie_watchlist = MovieWatchlist.find_by(title: search)
-    if movie_watchlist
-      self.where(movie_watchlist_id: movie_watchlist)
-    else
-      MovieWatchlist.all
-    end
-  else
-    MovieWatchlist.all
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  root to: "pages#preferences"
+  root to: "pages#movies"
+  root to: "pages#preferences_movies"
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
