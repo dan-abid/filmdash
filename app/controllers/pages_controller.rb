36a1e75675946bbc2b34388b3cb60f7e5ec716c2
David Cazal <da.cazal95@gmail.com>
2024-12-10 13:54:36 -0500

    genre id changed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,14 +19,15 @@ class PagesController < ApplicationController
     release_date_end = params[:period].to_i + 100000
     @release_date_end_s = release_date_end.to_s
 
+    seed ={}
     url = "https://api.watchmode.com/v1/list-titles/?apiKey=#{ENV["API_KEY_WATCHMODE"]}&types=#{params[:format]}&source_ids=#{@streaming_services_ids}&source_types=sub&region=#{@country}&genres=#{params[:genre]}&release_date_start=#{params[:period]}&release_date_end=#{@release_date_end_s}&critic_score_low=8&limit=250"
 
-    response = RestClient.get(url)
-    @result_tt = JSON.parse(response)
-    if @result_tt == []
-
+    if seed.key?(url) == false
+      response = RestClient.get(url)
+      result_tt = JSON.parse(response)
+      seed[url] = result_tt
     end
-    @result_titles = @result_tt["titles"].sample(3)
+    @result_titles = result_tt["titles"].sample(3)
     # raise
     @result = @result_titles.map do |title_infos|
       titles_details_parse = RestClient.get "https://api.watchmode.com/v1/title/#{title_infos["imdb_id"]}/details/?apiKey=#{ENV["API_KEY_WATCHMODE"]}&append_to_response=sources"
