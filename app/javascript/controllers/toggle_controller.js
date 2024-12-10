0e5d6439e26d6c308d67f7bafc4a2f85d6ac1d1f
David Cazal <da.cazal95@gmail.com>
2024-12-09 19:37:39 -0500

    clic on a movie and it close the others informations movie

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -15,11 +15,45 @@ export default class extends Controller {
 
   connect() {
     console.log("Hello");
+    document.addEventListener("click", this.handleOutsideClick.bind(this));
 
+  }
+
+  disconnect () {
+    document.removeEventListener("click", this.handleOutsideClick.bind(this));
+  }
+
+  handleOutsideClick(event) {
+    // Vérifier si l'élément cliqué est en dehors des éléments que nous voulons fermer
+    if (
+      !this.container1ElementTarget.contains(event.target) &&
+      !this.container2ElementTarget.contains(event.target) &&
+      !this.container3ElementTarget.contains(event.target)
+    ) {
+      this.closeAll();
+    }
+  }
+
+  closeAll() {
+    this.appear1ElementTarget.classList.add("d-none");
+    this.play1ElementTarget.classList.add("d-none");
+    this.container1ElementTarget.classList.remove("justify-content-between");
+    this.container1ElementTarget.classList.add("justify-content-center");
+
+    this.appear2ElementTarget.classList.add("d-none");
+    this.play2ElementTarget.classList.add("d-none");
+    this.container2ElementTarget.classList.remove("justify-content-between");
+    this.container2ElementTarget.classList.add("justify-content-center");
+
+    this.appear3ElementTarget.classList.add("d-none");
+    this.play3ElementTarget.classList.add("d-none");
+    this.container3ElementTarget.classList.remove("justify-content-between");
+    this.container3ElementTarget.classList.add("justify-content-center");
   }
 
   button1 () {
     // event.target.classList.add("movie-card-dark")
+    this.closeAll();
     this.appear1ElementTarget.classList.remove("d-none");
     this.play1ElementTarget.classList.remove("d-none");
     this.container1ElementTarget.classList.remove("justify-content-center");
@@ -28,6 +62,7 @@ export default class extends Controller {
   }
 
   button2 () {
+    this.closeAll();
     this.appear2ElementTarget.classList.remove("d-none");
     this.play2ElementTarget.classList.remove("d-none");
     this.container2ElementTarget.classList.remove("justify-content-center");
@@ -35,6 +70,7 @@ export default class extends Controller {
   }
 
   button3 () {
+    this.closeAll();
     this.appear3ElementTarget.classList.remove("d-none");
     this.play3ElementTarget.classList.remove("d-none");
     this.container3ElementTarget.classList.remove("justify-content-center");
