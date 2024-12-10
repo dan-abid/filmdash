43c127bd762ce2dc280ee089bab2e2b101db932c
danial abid <dan.abid91@gmail.com>
2024-12-10 12:01:47 -0500

    updates done

--- app/controllers/movie_watchlist_controller.rb
+++ app/controllers/movie_watchlist_controller.rb
@@ -1,2 +1,25 @@
 class MovieWatchlistController < ApplicationController
+
+  def index
+    @movies = MovieWatchlist.all
   end
+
+def add
+  @movie_watchlist = MovieWatchlist.new(movie_params)
+  if @movie_watchlist.save
+    redirect_to @movie
+  else
+    render :new
+  end
+end
+
+def show
+  @movie = MovieWatchlist.find(params[:id])
+end
+
+def delete
+  @movie = MovieWatchlist.find(params[:id])
+  @movie.destroy
+
+  redirect_to movie_watchlist_index_path
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  root to: "pages#preferences"
-  root to: "pages#movies"
-  root to: "pages#preferences_movies"
-end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
   # root "posts#index"
   get "/preferences", to: "pages#preferences", as: :preferences
   get "/preferences/movies", to: "pages#movies", as: :preferences_movies
+  get "/watchlist", to: "movie_watchlist#index", as: :watchlist
+  get "/watchlist/:id", to: "movie_watchlist#show", as: :watchlist_movie
 end
