e49b0ed1e2696dfa1b26b216aebe054814ba2977
danial abid <dan.abid91@gmail.com>
2024-12-12 14:45:51 -0500

    updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -115,8 +115,9 @@ class PagesController < ApplicationController
   end
 
   def prepare_result(full_results)
-    final_result = full_results.slice("backdrop_path", "id", "overview", "poster_path", "release_date", "title", "vote_average", "runtime")
+    final_result = full_results.slice("backdrop_path", "overview", "poster_path", "release_date", "title", "vote_average", "runtime")
     final_result["genre"] = @genre
+    final_result["tmdb_id"] = full_results["id"]
     watch_providers = full_results["watch/providers"]["results"][@country]
     final_result["streaming_link"] = watch_providers["link"]
     final_result["watch_providers"] = watch_providers["flatrate"]
